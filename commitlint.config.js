480b575119d829ad765ab42c2bcd44441cf72780
kengnidoriane <dorianekengni@gmail.com>
2025-12-19 16:47:01 +0100

    fix: update commitlint config and package.json formatting

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,5 +1,4 @@
 module.exports = {
-  extends: ['@commitlint/config-conventional'],
   rules: {
     'type-enum': [
       2,
@@ -18,10 +17,7 @@ module.exports = {
         'revert',
       ],
     ],
-    'subject-case': [2, 'never', ['sentence-case', 'start-case', 'pascal-case', 'upper-case']],
     'subject-empty': [2, 'never'],
-    'subject-full-stop': [2, 'never', '.'],
-    'type-case': [2, 'always', 'lower-case'],
     'type-empty': [2, 'never'],
   },
 };
